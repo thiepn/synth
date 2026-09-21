ec196a4d22fef2f3251635648b6d495f9319ecef
thiepn <junsosmart@gmail.com>
2026-09-21 15:37:30 +0200

    style: load Drum Engine V1 surface

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 import "./transport.css";
+import "./drums.css";
 
 const root = document.getElementById("root");
 
