f37265e844d65e3ccc6adaa1ae28a3b435e99b6b
thiepn <junsosmart@gmail.com>
2026-09-22 04:29:37 +0200

    chore: advance Synth shell to Phase 13

--- src/App.tsx
+++ src/App.tsx
@@ -88,7 +88,7 @@ function ModeRail({
       ))}
       <div className="mode-rail__system">
         <span className="status-lamp" />
-        <span>PHASE 12</span>
+        <span>PHASE 13</span>
       </div>
     </nav>
   );
