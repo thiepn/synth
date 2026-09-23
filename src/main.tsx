06d18b27b18bfc11ce8b3aa1a29aff06b029ddb7
thiepn <junsosmart@gmail.com>
2026-09-23 18:49:13 +0200

    style(project): load project persistence controls

--- src/main.tsx
+++ src/main.tsx
@@ -24,6 +24,7 @@ import "./master-export.css";
 import "./sample-lab.css";
 import "./resample.css";
 import "./midi.css";
+import "./project.css";
 import "./song.css";
 
 const root = document.getElementById("root");
